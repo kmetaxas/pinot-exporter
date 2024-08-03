a242ee480352eeec6e6833e5063e09c5485365cc
Konstantinos Metaxas <kmetaxas@gmail.com>
2024-08-03 20:55:23 +0200

    add test for table cache

---
+++ table_cache_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTableRefreshChanListener(t *testing.T) {
+	tables := make(chan []string, 1)
+
+	var cache TableCache
+
+	go cache.TableRefreshChanListener(tables)
+	table_list1 := []string{"trololo", "skata"}
+	tables <- table_list1
+	// This refresh is not instant and goes through the channel etc. Wait 50m s before testing
+	time.Sleep(time.Duration(50) * time.Millisecond)
+	assert.Equal(t, cache.GetTables(), table_list1)
+
+	table_list2 := []string{"trololo"}
+	tables <- table_list2
+	// This refresh is not instant and goes through the channel etc. Wait 50m s before testing
+	time.Sleep(time.Duration(50) * time.Millisecond)
+	assert.Equal(t, cache.GetTables(), table_list2)
+}
